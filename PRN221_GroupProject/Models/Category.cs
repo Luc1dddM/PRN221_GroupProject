ffa38b0c445712806d33e9230f67bee8dc8b2b55
Hvuthai <hoangvuthai.bl1@gmail.com>
2024-05-10 23:07:11 +0700

    CRUD for category and product(not finish)

---
+++ PRN221_GroupProject/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PRN221_GroupProject.Models;
+
+public partial class Category
+{
+    public int Id { get; set; }
+
+    public string CategoryId { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string Type { get; set; } = null!;
+
+    public string CreatedBy { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public string UpdatedBy { get; set; } = null!;
+
+    public DateTime UpdatedAt { get; set; }
+
+    public bool Status { get; set; }
+}
---
+++ PRN221_GroupProject/Pages/Categories/Detail.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PRN221_GroupProject.Models;
+
+namespace PRN221_GroupProject.Pages.Categories
+{
+    public class DetailModel : PageModel
+    {
+        private readonly PRN221_GroupProject.Models.Prn221GroupProjectContext _context;
+
+        public DetailModel(PRN221_GroupProject.Models.Prn221GroupProjectContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Category Category { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category =  await _context.Categories.FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            Category = category;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Category).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(Category.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _context.Categories.Any(e => e.Id == id);
+        }
+    }
+}
