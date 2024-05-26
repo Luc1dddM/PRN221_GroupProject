431852b65b764b355d79491a19e468899afb7ac1
Phát Tu <phattqce170538@fpt.edu.vn>
2024-05-26 18:49:53 +0700

    crudCartDone

--- PRN221_GroupProject/Pages/Cart/ShoppingCart.cshtml.cs
+++ PRN221_GroupProject/Pages/Cart/ShoppingCart.cshtml.cs
@@ -19,54 +19,35 @@ namespace PRN221_GroupProject.Pages.Cart
         [BindProperty]
         public CartDetail CartDetailPostModel { get; set; } = default!; //handle input from view
 
-        public async Task LoadCartDetails()
+        public async Task OnGetAsync() //currently Get all cartdetail, since there is no user identifier 
         {
             CartDetail = await _context.CartDetails.AsNoTracking()
                 .Include(cd => cd.Cart)
                 .Include(cd => cd.Product)
                 .ToListAsync();
-                /*.ThenInclude(p => p.ProductCategories)
-                .ThenInclude(pc => pc.Category)*/
         }
 
-        public async Task OnGetAsync() //currently Get all cartdetail, since there is no user identifier 
+        public async Task<IActionResult> OnPostUpdateAsync()
         {
-            await LoadCartDetails();
-        }
-
-        public async Task<IActionResult> OnPostUpdateCartAsync()
-        {
-            await LoadCartDetails();
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
 
             var existingCart = await _context.CartDetails.FirstOrDefaultAsync(cd => cd.CartDetail1 == CartDetailPostModel.CartDetail1 &&
-                                                                                cd.ProductId == CartDetailPostModel.ProductId);
+                                                                            cd.ProductId == CartDetailPostModel.ProductId);
             if (existingCart != null)
             {
                 existingCart.Count = CartDetailPostModel.Count;
                 _context.CartDetails.Update(existingCart);
                 await _context.SaveChangesAsync();
             }
-
-
-
             return RedirectToPage();
         }
 
-        public async Task<IActionResult> OnPostDeteleProductAsync(string id)
+        public async Task<IActionResult> OnPostRemoveAsync()
         {
-            //remove product in cart
-            if (!string.IsNullOrEmpty(id))
+            var productRemove = await _context.CartDetails.FirstOrDefaultAsync(cd => cd.CartDetail1 == CartDetailPostModel.CartDetail1);
+            if (productRemove != null)
             {
-                var productRemove = await _context.CartDetails.FirstOrDefaultAsync(cd => cd.ProductId == id);
-                if (productRemove != null)
-                {
-                    _context.CartDetails.Remove(productRemove);
-                    await _context.SaveChangesAsync();
-                }
+                _context.CartDetails.Remove(productRemove);
+                await _context.SaveChangesAsync();
             }
             return RedirectToPage();
         }
