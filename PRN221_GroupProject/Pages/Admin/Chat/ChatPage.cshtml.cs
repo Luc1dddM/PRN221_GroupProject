a48b6ef069663826f3957932876454ab3b5a0ed9
Hvuthai <hoangvuthai.bl1@gmail.com>
2024-07-05 22:42:15 +0700

    Chatpage for admin

---
+++ PRN221_GroupProject/Pages/Admin/Chat/ChatPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221_GroupProject.Pages.Admin.Chat
+{
+    public class ChatPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
